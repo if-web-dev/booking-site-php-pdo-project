cf420041e764d2a970621b03af819638cd9e1f1a
FOUHAL <i.fouhal@hotmail.com>
2022-02-03 20:17:08 +0100

    hotel form modifications

--- views/hotels.view.php
+++ views/hotels.view.php
@@ -3,7 +3,7 @@
 
   <div class="row justify-content-center">
 
-    <section class="my-4 col-12 col-lg-6 pl-lg-5 pr-lg-5">
+    <section class="my-4 col-12 col-lg-6">
 
       <ul class="list-unstyled">
         <?php
@@ -37,7 +37,7 @@
 
     </section>
 <!--form reservation-->  
-    <section class="form-container col-12 col-lg-3 booking mb-6">
+    <section class="form-container col-12 col-md-6 booking mb-6">
 
       <div class="form-body" id="hotel-form">
           
@@ -46,47 +46,55 @@
                 <div class="form-items shadow">
                     <h3>Réservation</h3>
                     <p>Les champs (*) sont obligatoire.</p>
-                    <form class="requires-validation" method="post" action="hotels" novalidate>
+                    <form class="requires-validation row" method="post" action="hotels" novalidate>
 
-                      <div class="col-md-12">
-                          <label for="hotel_name">Choix de l'hotel * :</label>
-                          <select class="form-control" name="hotel_id" id="hotel_name" required>
-                              <option value="">--Choisissez un hotel--</option>
-                              
-                              <?php foreach($results as $result):?>
-                              <option value="<?php echo $result['id'] ;?>"><?php echo $result['hotel_names'] ;?></option>
-                              <?php endforeach;?>
-                          </select>
+                      <div class="col-md-12 col-lg-6">
+                        
+                        <div class="col-md-12">
+                            <label for="hotel_name">Choix de l'hotel * :</label>
+                            <select class="form-control" name="hotel_id" id="hotel_name" required>
+                                <option value="">--Choisissez un hotel--</option>
+                                
+                                <?php foreach($results as $result):?>
+                                <option value="<?php echo $result['id'] ;?>"><?php echo $result['hotel_names'] ;?></option>
+                                <?php endforeach;?>
+                            </select>
+                        </div>
+
+                        <div class="col-md-12 mt-3">
+                          <label>Date de début * :</label>
+                          <input class="form-control" type="date" name="date_start" id="date_start" min="<?php echo $today ?>" placeholder="Full Name" required>
+                        </div>
+
+                        <div class="col-md-12 mt-3">
+                            <label>Date de fin * :</label>
+                            <input class="form-control" type="date" name="date_end" id="date_end" min="<?php echo $today ?>" placeholder="Full Name" required>  
+                        </div>
+
                       </div>
 
-                      <div class="col-md-12 mt-3">
-                        <label>Date de début * :</label>
-                        <input class="form-control" type="date" name="date_start" id="date_start" min="<?php echo $today ?>" placeholder="Full Name" required>
+                      <div class=" col-md-12 col-lg-6">
+
+                        <div class="col-md-12">
+                            <label for="user_name">Votre nom * :</label>
+                            <input class="form-control" type="text" name="user_name" placeholder="Nom" required>
+                        </div>
+
+
+                        <div class="col-md-12 mt-3">
+                            <label>Votre adresse e-mail * :</label>
+                            <input class="form-control" type="email" name="user_mail" placeholder="Exemple@Exemple.fr" required>
+                        </div>
+
+                        <div class="form-button mt-5">
+                          <input type="submit" name="submit" class="btn btn-primary"></input>
+                          <input type="reset" class="btn btn-primary"></input>
+                        </div>
+
+                        <?php if(isset($message)){echo "<p class='my-2' style='text-align: justify; color: #e2d273;'>".htmlspecialchars($message)."</p>";}?>
+
                       </div>
 
-                      <div class="col-md-12 mt-3">
-                          <label>Date de fin * :</label>
-                          <input class="form-control" type="date" name="date_end" id="date_end" min="<?php echo $today ?>" placeholder="Full Name" required>  
-                      </div>
-
-                      <div class="col-md-12 mt-3">
-                          <label for="user_name">Votre nom * :</label>
-                          <input class="form-control" type="text" name="user_name" placeholder="Nom" required>
-                      </div>
-
-
-                      <div class="col-md-12 mt-3">
-                          <label>Votre adresse e-mail * :</label>
-                          <input class="form-control" type="email" name="user_mail" placeholder="Exemple@Exemple.fr" required>
-                      </div>
-
-                      <div class="form-button mt-5">
-                        <input type="submit" name="submit" class="btn btn-primary"></input>
-                        <input type="reset" class="btn btn-primary"></input>
-                      </div>
-
-                      <?php if(isset($message)){echo "<p class='my-2' style='text-align: justify; color: #e2d273;'>".htmlspecialchars($message)."</p>";}?>
-        
                     </form>
                 </div>
             </div>
