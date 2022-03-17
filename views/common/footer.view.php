ec9556630bab63d7dbad772f634122ef328b1fdc
ishake fouhal <i.fouhal@hotmail.com>
2022-03-18 00:47:45 +0100

    new footer

--- views/common/footer.view.php
+++ views/common/footer.view.php
@@ -1,7 +1,37 @@
 
 
-<footer class="py-5 text-white violet-bg text-center">
+<footer class="py-5 text-white violet-bg ">
+	<div class="row container-fluid justify-content-center">
+		<div class="col-5 col-xl-3">
+			<ul>
+				<li>
+					<a href="accueil" class="text-decoration-none text-dark">Home</a>
+				</li>
+				<li>
+					<a href="hotels" class="text-decoration-none text-dark">Hotels</a>
+					
+				</li>
+				<li>
+					<a href="contacts" class="text-decoration-none text-dark">Contacts</a>
+				</li>
+			</ul>
+		</div>
+		<div class="col-5 col-xl-3">
+			<ul>
+				<li>
+					<a href="#" class="text-decoration-none text-dark">Mentions légales</a>
+				</li>
+				<li>
+					<a href="#" class="text-decoration-none text-dark">Conditions générales de Vente</a>
+				</li>
+				<li>
+					<a href="#" class="text-decoration-none text-dark">Mon compte</a>
+				</li>
+			</ul>
+		</div>
+		<div class="col-12 col-xl-3">
+			<p class="display-4 m-0 text-center">BOOKING site</p>
+		</div>
+	</div>
 	
-	<a href="#" class="text-white">footer</a>
-   
 </footer>
