6d2dd25ad3cd2b7e166f71839062d04c084b2204
ishake fouhal <i.fouhal@hotmail.com>
2022-05-10 19:55:29 +0200

    footer improvement

--- views/common/footer.view.php
+++ views/common/footer.view.php
@@ -1,7 +1,10 @@
-<footer class="py-5 text-white violet-bg ">
-	<div class="row container-fluid justify-content-center">
-		<div class="col-12 col-xl-3">
+<footer class="text-white violet-bg ">
+	<div class="py-5 row container-fluid justify-content-center">
+		<div class="col-12 col-md-3">
 			<ul>
+				<li>
+					<h4>Plan du site</h4>
+				</li>
 				<li>
 					<a href="accueil" class="text-decoration-none text-dark">Home</a>
 				</li>
@@ -14,8 +17,11 @@
 				</li>
 			</ul>
 		</div>
-		<div class="col-12 col-xl-3">
+		<div class="col-12 col-md-3">
 			<ul>
+				<li>
+					<h4>Liens utiles</h4>
+				</li>
 				<li>
 					<a href="#" class="text-decoration-none text-dark">Mentions légales</a>
 				</li>
@@ -27,8 +33,11 @@
 				</li>
 			</ul>
 		</div>
-		<div class="col-12 col-xl-3">
+		<div class="col-12 col-md-3">
 			<p class="display-4 m-0 text-center">BOOKING site</p>
 		</div>
+	</div>
+	<div class="bg-dark text-center">
+		<p class="m-0 py-2">Copyright © Booking-site | Designed by If-web-dev</p>
 	</div>
 </footer>
